7fe276194a5b3cd4de68678f90c8df93b33b0060
souravion <40751230+souravion@users.noreply.github.com>
2023-07-25 00:17:35 +0530

    change code

--- src/js/sdk.js
+++ src/js/sdk.js
@@ -1,57 +1,56 @@
 class IconSDK {
-    constructor(icons, containerId, buttonId) {
-      this.icons = icons;
-      this.containerId = containerId;
-      this.buttonId = buttonId;
-    }
-  
-    init() {
-      const container = document.getElementById(this.containerId);
-      const button = document.getElementById(this.buttonId);
-      const handleClickOutside = event => {
-        if (!container.contains(event.target) && event.target !== button) {
-          container.classList.remove("show"); 
-        }
-      };
-
-      button.addEventListener("click", () => {
-        alert("hi")
-        container.classList.toggle("show"); 
-      });
-  
-      document.addEventListener("click", handleClickOutside);
-
-      this.icons.forEach(icon => {
-        const iconLink = document.createElement("a"); 
-        iconLink.classList.add("icon-link");
-        iconLink.href = icon.url; 
-        iconLink.target = "_blank"; 
-  
-        const iconElement = document.createElement("div");
-        iconElement.classList.add("icon");
-  
-        const iconImage = document.createElement("img");
-        iconImage.src = icon.icon;
-        iconImage.alt = icon.title;
-        iconElement.appendChild(iconImage);
-        const iconTitle = document.createElement("p");
-        iconTitle.innerText = icon.title;
-        iconElement.appendChild(iconTitle); 
-  
-        iconLink.appendChild(iconElement); 
-        container.appendChild(iconLink); 
-      });
-    }
+  constructor(icons, containerId, buttonId) {
+    this.icons = icons;
+    this.containerId = containerId;
+    this.buttonId = buttonId;
   }
-  
-  const icons = [
-    { title: "StockEdge WebApp", icon: "https://web.stockedge.com/assets/img/stockedge-products/stockedge-logo.png", url: "https://web.stockedge.com/app/equities" },
-    { title: "StockEdge Social", icon: "https://web.stockedge.com/assets/img/stockedge-products/se-social-logo.png", url: "https://social.stockedge.com/" },
-    { title: "StockEdge Website", icon: "https://web.stockedge.com/assets/img/stockedge-products/stockedge-logo.png", url: "https://web.stockedge.com/app/equities" },
-  ];
-  
-  const containerId = "icon-list";
-  const buttonId = "show-icons-button";
-  const iconSDK = new IconSDK(icons, containerId, buttonId);
-  iconSDK.init();
-  
+
+  init() {
+    const container = document.getElementById(this.containerId);
+    const button = document.getElementById(this.buttonId);
+    const handleClickOutside = (event) => {
+      if (!container.contains(event.target) && event.target !== button) {
+        container.classList.remove("show");
+      }
+    };
+
+    button.addEventListener("click", () => {
+      container.classList.toggle("show");
+    });
+
+    document.addEventListener("click", handleClickOutside);
+
+    this.icons.forEach((icon) => {
+      const iconLink = document.createElement("a");
+      iconLink.classList.add("icon-link");
+      iconLink.href = icon.url;
+      iconLink.target = "_blank";
+
+      const iconElement = document.createElement("div");
+      iconElement.classList.add("icon");
+
+      const iconImage = document.createElement("img");
+      iconImage.src = icon.icon;
+      iconImage.alt = icon.title;
+      iconElement.appendChild(iconImage);
+
+      const iconTitle = document.createElement("p");
+      iconTitle.innerText = icon.title;
+      iconElement.appendChild(iconTitle);
+
+      iconLink.appendChild(iconElement);
+      container.appendChild(iconLink);
+    });
+  }
+}
+
+const icons = [
+  { title: "StockEdge WebApp", icon: "https://web.stockedge.com/assets/img/stockedge-products/stockedge-logo.png", url: "https://web.stockedge.com/app/equities" },
+  { title: "StockEdge Social", icon: "https://web.stockedge.com/assets/img/stockedge-products/se-social-logo.png", url: "https://social.stockedge.com/" },
+  { title: "StockEdge Website", icon: "https://web.stockedge.com/assets/img/stockedge-products/stockedge-logo.png", url: "https://web.stockedge.com/app/equities" },
+];
+
+const containerId = "icon-list";
+const buttonId = "show-icons-button";
+const iconSDK = new IconSDK(icons, containerId, buttonId);
+iconSDK.init();
